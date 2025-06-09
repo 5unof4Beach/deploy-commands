5e1735493fff051cbb6520da1ccd088636bc7fcb
Duc Bui <suckmydick2001gta@gmail.com>
2025-06-09 22:30:09 +0700

    update github action deploy script

--- nestjs-template/github-action-deploy.sh
+++ nestjs-template/github-action-deploy.sh
@@ -12,7 +12,7 @@ env=${1:-stage}
 
     docker compose -f ./prod/docker-compose.yml down api
     docker pull $IMAGE_NAME
-    docker image rm $IMAGE_NAME || true
+
 
     if [[ "$env" == "production" ]]; then
         docker compose -f ./prod/docker-compose.yml up -d api
