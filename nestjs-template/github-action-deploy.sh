34f7879a08d334e573b549cf79f9e16783869313
Duc Bui <suckmydick2001gta@gmail.com>
2025-06-09 21:49:01 +0700

    initial commit

---
+++ nestjs-template/github-action-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+env=${1:-stage}
+
+{
+
+    if [ $? != 0 ] ; then
+        exit 1
+    fi
+
+    IMAGE_NAME="5unof4beach/nestjs-boiler-plate:latest"
+
+    docker compose -f ./prd/docker-compose.yml down api
+    docker pull $IMAGE_NAME
+    docker image rm $IMAGE_NAME || true
+
+    if [[ "$env" == "production" ]]; then
+        docker compose -f ./prd/docker-compose.yml up -d api
+    elif [[ "$env" == "stage" ]]; then
+        docker compose -f ./stg/docker-compose.yml up -d api
+    else
+        echo "Invalid env!"
+        exit 1
+    fi
+} || {
+    echo 'Failed. Aborted!'
+}
